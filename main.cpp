33353df3bf2b0310cfcdce6d09d815fe8346e976
bruce-bst <liu_wei@bluestacks.com>
2021-12-27 21:57:08 +0800

    Enable hidpi

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@
 
 int main(int argc, char *argv[])
 {
+
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 6, 0))
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
     SingleApplication a(argc, argv);
     if (a.isRunning()) {
         return 0;
